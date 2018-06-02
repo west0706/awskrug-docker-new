bc3cafa25e996d1d6f697c2602649510696380b7
west0706 <west0706@gmail.com>
2018-06-02 07:08:03 +0000

    test

--- node/server.js
+++ node/server.js
@@ -10,8 +10,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send("<html>Version 3<head><h1>hostname : " + hostname +
-        "</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
+  res.send("Hello world!!!");
 });
 
 app.listen(PORT, HOST);
